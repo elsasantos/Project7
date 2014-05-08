098c1136f1473c1ff9a5c6932cfc972823c0ba56
Aires <airesv@gmail.com>
2014-05-08 17:51:23 +0100

    primeiro

---
+++ WebServiceAPI/src/java/pt/uc/aor/webservice/facade/MakingSell.java
@@ -0,0 +1,60 @@
+/*
+ * Projeto 7 -TecnoAPI
+ * Elsa Santos & VitorAires  *
+ */
+
+package pt.uc.aor.webservice.facade;
+
+import java.util.HashMap;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.apache.commons.logging.Log;
+import pt.uc.aor.webservice.entity.Client;
+import pt.uc.aor.webservice.entity.Product;
+import pt.uc.aor.webservice.entity.Sell;
+import pt.uc.aor.webservice.entity.SellProduct;
+
+/**
+ *
+ * @author Aires
+ */
+@Stateless
+public class MakingSell {
+
+    @PersistenceContext(unitName = "WebServicePU")
+    private EntityManager em;
+
+    Log log;
+
+    @Inject
+    private ClientFacade cf;
+    @Inject
+    private ProductFacade pf;
+    @Inject
+    private SellFacade sf;
+    @Inject
+    private SellProductFacade spf;
+
+    private void makeSell(HashMap<Integer, Integer> hashmap, String apkKey) {
+        Client buyer;
+        if (cf.existApi(apkKey)) {
+            Sell sell = new Sell();
+            //sell.setClientidClient(buyer.getIdClient());
+            Product product;
+            for (Integer idproduto : hashmap.keySet()) {
+                product = pf.find(idproduto);
+                SellProduct sellProduct = new SellProduct(hashmap.get(idproduto), sell, product);
+                sell.getSellProductList().add(sellProduct);
+                product.getSellProductList().add(sellProduct);
+            }
+
+        } else {
+            System.out.println("Não existe este Api");
+
+        }
+
+    }
+
+}
