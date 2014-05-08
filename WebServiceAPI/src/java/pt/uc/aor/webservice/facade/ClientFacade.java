eb6a883fd967ef2c2b1e08dc5e15a299f5727162
Aires <airesv@gmail.com>
2014-05-08 21:59:45 +0100

    primeiro

--- WebServiceAPI/src/java/pt/uc/aor/webservice/facade/ClientFacade.java
+++ WebServiceAPI/src/java/pt/uc/aor/webservice/facade/ClientFacade.java
@@ -8,6 +8,7 @@ package pt.uc.aor.webservice.facade;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import pt.uc.aor.webservice.entity.Client;
 
 /**
@@ -16,6 +17,7 @@ import pt.uc.aor.webservice.entity.Client;
  */
 @Stateless
 public class ClientFacade extends AbstractFacade<Client> {
+
     @PersistenceContext(unitName = "WebServicePU")
     private EntityManager em;
 
@@ -26,6 +28,23 @@ public class ClientFacade extends AbstractFacade<Client> {
 
     public ClientFacade() {
         super(Client.class);
+    }
+
+    public Client findClientByApi(String api) {
+        Query query = em.createNamedQuery("Client.findByApikey", Client.class).setParameter("apikey", api);
+        return (Client) query.getSingleResult();
+    }
+
+    public Boolean existApi(String api) {
+
+        Query query = em.createNamedQuery("Client.findByApikey", Client.class).setParameter("apikey", api);
+
+        if (query.getMaxResults() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
 }
