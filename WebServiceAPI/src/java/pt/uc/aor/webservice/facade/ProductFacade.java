7c09e28b394259a8662b2f125663f8cb8219feb9
elsasantos <elsateodoros@gmail.com>
2014-05-10 18:27:23 +0100

    Merge origin/master
    Conflicts:
    	WebServiceAPI/src/java/pt/uc/aor/webservice/facade/ProductFacade.java

--- WebServiceAPI/src/java/pt/uc/aor/webservice/facade/ProductFacade.java
+++ WebServiceAPI/src/java/pt/uc/aor/webservice/facade/ProductFacade.java
@@ -12,6 +12,7 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import pt.uc.aor.webservice.entity.Category;
 import pt.uc.aor.webservice.entity.Product;
 
@@ -37,17 +38,39 @@ public class ProductFacade extends AbstractFacade<Product> {
         super(Product.class);
     }
 
-    public List<Product> findByCategory(Long idCategory) {
-        log.info("Product.findByCategoriaidCategoria(" + idCategory + ")");
-        List<Product> p = null;
+    /**
+     * Lista todos os produtos existentes na entidade cujo stock é superior a
+     * zero
+     *
+     * @return Lista dos produtos
+     */
+    public List<Product> findAllProducts() {
+        log.info("Product.findAllProducts");
+        List<Product> p = new ArrayList<>();
+        try {
+            p = em.createNamedQuery("Product.findAll").getResultList();
+        } catch (NoResultException ex) {
+            log.info("Não encontrou nenhum Produto com Stock superior a zero.");
+        }
+        return p;
+    }
 
+    /**
+     * Lista os produtos por categoria
+     *
+     * @param idCategory
+     * @return
+     */
+    public List<Product> findProductByCategory(Long idCategory) {
+        log.info("Product.findProductByCategoria(" + idCategory + ")");
+        List<Product> p = new ArrayList<>();
         try {
             Category c = cf.find(idCategory);
-            return p = em.createNamedQuery("Product.findByCategoriaidCategoria").setParameter("categoria", c).getResultList();
+            p = em.createNamedQuery("Product.findByCategoriaidCategoria").setParameter("categoria", c).getResultList();
         } catch (NoResultException ex) {
             log.info("Não encontrou nenhum Categoria'" + idCategory + "'.");
         }
-        return p = new ArrayList<>();
+        return p;
     }
 
     /**
@@ -63,7 +86,6 @@ public class ProductFacade extends AbstractFacade<Product> {
         try {
             if (column.equals("Designation")) {
                 p = em.createNamedQuery("Product.findByWord").setParameter("word", "%" + word + "%").getResultList();
-
             }
             if (column.equals("Category")) {
                 p = em.createNamedQuery("Product.findByCategoriaName").setParameter("word", "%" + word + "%").getResultList();
@@ -73,7 +95,46 @@ public class ProductFacade extends AbstractFacade<Product> {
             }
 
         } catch (NoResultException ex) {
-            log.info("Não encontrou nenhum Produto '" + word + "'.");
+            log.info("Não encontrou nenhum Produto pela pesquisa '" + word + "'.");
+        }
+        return p;
+    }
+
+    /**
+     * Lista os detalhes de um determinado produto pelo seu Id
+     *
+     * @param id
+     * @return
+     */
+    public Product findProductById(Long id) {
+        log.info("Product.findProductById(" + id + ")");
+        Product p = new Product();
+        try {
+            p = super.find(id);
+        } catch (NoResultException ex) {
+            log.info("Não encontrou nenhum Produto com o Id " + id + ".");
+        }
+        return p;
+    }
+
+    /**
+     * Lista os detalhes do produto segundo os atributos da designação
+     *
+     * @param brand
+     * @param model
+     * @param version
+     * @return
+     */
+    public Product findProductByDesignation(String brand, String model, String version) {
+        log.info("Product.findProductByDesignation(" + brand + ", " + model + " and " + version + ".)");
+        TypedQuery<Product> q;
+        Product p = new Product();
+        try {
+            q = em.createNamedQuery("Product.findByDesignation", Product.class);
+            q.setParameter("brand", "%" + brand + "%").setParameter("model", "%" + model + "%").setParameter("version", "%" + version + "%");
+            p = q.getSingleResult();
+        } catch (NoResultException ex) {
+            log.info("Não encontrou nenhum Produto com a designação " + brand + ", " + model + " e " + version + ".");
         }
         return p;
     }
